d91e880ed99feea69d5874f8e2442c0fb564c056
GwynethS <102232553+GwynethS@users.noreply.github.com>
2023-08-21 23:18:03 -0500

    AAD - Ejercicio 1

---
+++ Semana 1/Ejercicio 1/Ejercicio 1/Empleado.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_1
+{
+    class Empleado
+    {
+        public Empleado() { }
+        public Empleado(String nombre, String categoria, int tardanzas, int llamadas)
+        {
+            Nombre = nombre;
+            Categoria = categoria;
+            Tardanzas = tardanzas;
+            Llamadas = llamadas;
+        }
+        public String Nombre { get; set; }
+        public String Categoria { get; set; }
+        public int Tardanzas { get; set; }
+        public int Llamadas { get; set; }
+
+        public double Pago { get; set; }
+    }
+}
