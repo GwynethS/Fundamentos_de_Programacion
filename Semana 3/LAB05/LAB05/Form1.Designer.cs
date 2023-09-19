76f620034abb9a634a93d4154692a8473c5a3b00
GwynethS <102232553+GwynethS@users.noreply.github.com>
2023-09-18 23:30:17 -0500

    Add files via upload

---
+++ Semana 3/LAB05/LAB05/Form1.Designer.cs
@@ -0,0 +1,335 @@
+﻿
+namespace LAB05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtPrecio = new System.Windows.Forms.TextBox();
+            this.txtBuscar = new System.Windows.Forms.TextBox();
+            this.numUpDownStock = new System.Windows.Forms.NumericUpDown();
+            this.btnRegistrar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnEliminarTodo = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.lbTotalRegistros = new System.Windows.Forms.Label();
+            this.lbTotalStock = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownStock)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CÓDIGO: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(398, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NOMBRE:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(27, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "STOCK:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(398, 69);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(63, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "PRECIO:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 177);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(166, 17);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "BUSCAR POR NOMBRE:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(518, 449);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(166, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "TOTAL DE REGISTROS:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(552, 480);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(132, 17);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "TOTAL EN STOCK:";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Location = new System.Drawing.Point(104, 21);
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.Size = new System.Drawing.Size(208, 22);
+            this.txtCodigo.TabIndex = 7;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(476, 21);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(267, 22);
+            this.txtNombre.TabIndex = 8;
+            // 
+            // txtPrecio
+            // 
+            this.txtPrecio.Location = new System.Drawing.Point(476, 64);
+            this.txtPrecio.Name = "txtPrecio";
+            this.txtPrecio.Size = new System.Drawing.Size(208, 22);
+            this.txtPrecio.TabIndex = 9;
+            // 
+            // txtBuscar
+            // 
+            this.txtBuscar.Location = new System.Drawing.Point(199, 172);
+            this.txtBuscar.Name = "txtBuscar";
+            this.txtBuscar.Size = new System.Drawing.Size(308, 22);
+            this.txtBuscar.TabIndex = 10;
+            // 
+            // numUpDownStock
+            // 
+            this.numUpDownStock.Location = new System.Drawing.Point(104, 64);
+            this.numUpDownStock.Name = "numUpDownStock";
+            this.numUpDownStock.Size = new System.Drawing.Size(116, 22);
+            this.numUpDownStock.TabIndex = 11;
+            // 
+            // btnRegistrar
+            // 
+            this.btnRegistrar.Location = new System.Drawing.Point(30, 123);
+            this.btnRegistrar.Name = "btnRegistrar";
+            this.btnRegistrar.Size = new System.Drawing.Size(116, 25);
+            this.btnRegistrar.TabIndex = 12;
+            this.btnRegistrar.Text = "Registrar";
+            this.btnRegistrar.UseVisualStyleBackColor = true;
+            this.btnRegistrar.Click += new System.EventHandler(this.btnRegistrar_Click);
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(180, 123);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(116, 25);
+            this.btnEliminar.TabIndex = 13;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnEliminarTodo
+            // 
+            this.btnEliminarTodo.Location = new System.Drawing.Point(345, 123);
+            this.btnEliminarTodo.Name = "btnEliminarTodo";
+            this.btnEliminarTodo.Size = new System.Drawing.Size(116, 25);
+            this.btnEliminarTodo.TabIndex = 14;
+            this.btnEliminarTodo.Text = "Eliminar todo";
+            this.btnEliminarTodo.UseVisualStyleBackColor = true;
+            this.btnEliminarTodo.Click += new System.EventHandler(this.btnEliminarTodo_Click);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(499, 123);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(116, 25);
+            this.btnOrdenar.TabIndex = 15;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(530, 171);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(96, 25);
+            this.btnBuscar.TabIndex = 16;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(632, 172);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(91, 25);
+            this.btnLimpiar.TabIndex = 17;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // lbTotalRegistros
+            // 
+            this.lbTotalRegistros.AutoSize = true;
+            this.lbTotalRegistros.Location = new System.Drawing.Point(693, 446);
+            this.lbTotalRegistros.Name = "lbTotalRegistros";
+            this.lbTotalRegistros.Size = new System.Drawing.Size(16, 17);
+            this.lbTotalRegistros.TabIndex = 18;
+            this.lbTotalRegistros.Text = "0";
+            // 
+            // lbTotalStock
+            // 
+            this.lbTotalStock.AutoSize = true;
+            this.lbTotalStock.Location = new System.Drawing.Point(693, 480);
+            this.lbTotalStock.Name = "lbTotalStock";
+            this.lbTotalStock.Size = new System.Drawing.Size(16, 17);
+            this.lbTotalStock.TabIndex = 19;
+            this.lbTotalStock.Text = "0";
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(30, 219);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(712, 195);
+            this.listView1.TabIndex = 20;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Código";
+            this.columnHeader1.Width = 99;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Nombre";
+            this.columnHeader2.Width = 209;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Stock";
+            this.columnHeader3.Width = 106;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Precio";
+            this.columnHeader4.Width = 114;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 525);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.lbTotalStock);
+            this.Controls.Add(this.lbTotalRegistros);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnEliminarTodo);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnRegistrar);
+            this.Controls.Add(this.numUpDownStock);
+            this.Controls.Add(this.txtBuscar);
+            this.Controls.Add(this.txtPrecio);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.txtCodigo);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numUpDownStock)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtPrecio;
+        private System.Windows.Forms.TextBox txtBuscar;
+        private System.Windows.Forms.NumericUpDown numUpDownStock;
+        private System.Windows.Forms.Button btnRegistrar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnEliminarTodo;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Label lbTotalRegistros;
+        private System.Windows.Forms.Label lbTotalStock;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Semana 3/LAB05/LAB05/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LAB05
+{
+    public partial class Form1 : Form
+    {
+        List<Electrodomestico> electrodomesticos = new List<Electrodomestico>(); 
+
+        public void Mostrar(List<Electrodomestico>lista)
+        {
+            listView1.Items.Clear();
+            int stockTotal = 0;
+
+            foreach (Electrodomestico ele in lista)
+            {
+                ListViewItem fila = new ListViewItem(ele.Codigo);
+                fila.SubItems.Add(ele.Nombre);
+                fila.SubItems.Add(ele.Stock.ToString());
+                fila.SubItems.Add(ele.Precio.ToString());
+                
+                listView1.Items.Add(fila);
+
+                stockTotal += ele.Stock;
+            }
+
+            //Actualizar los totales
+            lbTotalRegistros.Text = lista.Count.ToString();
+            lbTotalStock.Text = stockTotal.ToString();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistrar_Click(object sender, EventArgs e)
+        {
+            //Validación
+            if (txtCodigo.Text == "" || txtNombre.Text == "" || txtPrecio.Text == "" || numUpDownStock.Text == "")
+            {
+                MessageBox.Show("Por favor, complete todos los campos");
+                return;
+            }
+
+            //Verificar código repetido
+            bool existe = electrodomesticos.Exists(elemento => elemento.Codigo.Equals(txtCodigo.Text));
+            if (existe)
+            {
+                MessageBox.Show("El código ingresado ya existe.");
+                return;
+            }
+
+            //Crear el objeto
+            Electrodomestico elec = new Electrodomestico()
+            {
+                Codigo = txtCodigo.Text,
+                Nombre = txtNombre.Text,
+                Stock = (int)numUpDownStock.Value,
+                Precio = double.Parse(txtPrecio.Text)
+            };
+
+            //Agregar a la lista
+            electrodomesticos.Add(elec);
+
+            //Mostrar la lista
+            Mostrar(electrodomesticos);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            //Validación
+            if (listView1.SelectedItems.Count == 0)
+            {
+                MessageBox.Show("Por favor, seleccione un electrodoméstico");
+                return;
+            }
+
+            //Obtener el código
+            String codigo = listView1.SelectedItems[0].Text;
+
+            //Eliminar al que tiene ese código
+            electrodomesticos.RemoveAll(elememento => elememento.Codigo.Equals(codigo));
+
+            //Mostrar en el ListView
+            Mostrar(electrodomesticos);
+        }
+
+        private void btnEliminarTodo_Click(object sender, EventArgs e)
+        {
+            electrodomesticos.Clear();
+
+            Mostrar(electrodomesticos);
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            electrodomesticos.Sort((e1, e2) => {
+                if (e1.Precio < e2.Precio) return 1;
+                else return -1;
+            });
+
+            Mostrar(electrodomesticos);
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtBuscar.Text = "";
+            Mostrar(electrodomesticos);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            //Validación
+            if(txtBuscar.Text == "")
+            {
+                MessageBox.Show("Por favor, ingrese un nombre para iniciar la búsqueda");
+                return;
+            }
+
+            //Buscar el electrodoméstico
+            List<Electrodomestico> elecTemp = electrodomesticos.FindAll(elemento => elemento.Nombre.Contains(txtBuscar.Text));
+
+            Mostrar(elecTemp);
+        }
+    }
+}
