583de5a2ff9b90090ee2c50ba24e7185ded7d164
GwynethS <102232553+GwynethS@users.noreply.github.com>
2023-10-17 18:57:57 -0500

    Add files via upload

---
+++ Semana 10/Visual Studio Solution/Datos/Database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Datos
+{
+    public class Database
+    {
+        private SqlConnection conn;
+
+        public SqlConnection ConectaDb()
+        {
+            try
+            {
+                string CadenaDeConexion = "DataSource=SB605-17;Initial Catalog=SQL Server;Integrated Security=True";
+                conn = new SqlConnection(CadenaDeConexion);
+                conn.Open();
+                return conn;
+            }
+            catch(SqlException e)
+            {
+                return null;
+            }
+        }
+
+        public void DesconectaDb()
+        {
+            conn.Close();
+        }
+    }
+}
